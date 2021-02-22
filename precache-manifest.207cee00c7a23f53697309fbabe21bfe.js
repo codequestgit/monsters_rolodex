0270b4975a90427cca4c62fe2749d4527952c000
Naya <nayaumunna@gmail.com>
2021-02-22 17:17:00 +0100

    Updates

---
+++ precache-manifest.207cee00c7a23f53697309fbabe21bfe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "569aeeccfd2667c1100f",
+    "url": "/monsters_rolodex/static/css/main.3b84e10f.chunk.css"
+  },
+  {
+    "revision": "569aeeccfd2667c1100f",
+    "url": "/monsters_rolodex/static/js/main.9268984b.chunk.js"
+  },
+  {
+    "revision": "915468cfe8018d8d0c90",
+    "url": "/monsters_rolodex/static/js/runtime~main.c997bc71.js"
+  },
+  {
+    "revision": "886bbe336a570ed1e000",
+    "url": "/monsters_rolodex/static/js/2.6a952a6f.chunk.js"
+  },
+  {
+    "revision": "15fac73a2d8c565b77ca",
+    "url": "/monsters_rolodex/static/js/3.f37cd87c.chunk.js"
+  },
+  {
+    "revision": "d424f11b17d88c4acce87972f1ff489f",
+    "url": "/monsters_rolodex/index.html"
+  }
+];
